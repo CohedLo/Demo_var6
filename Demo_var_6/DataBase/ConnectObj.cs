01d790936113f8b7700670d9195f472bf9470d69
Amgalan03 <namsaraevar@mer.ci.nsu.ru>
2023-10-30 19:53:48 +0700

    Добавьте файлы проекта.

---
+++ Demo_var_6/DataBase/ConnectObj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_var_6.DataBase
+{
+    class ConnectObj
+    {
+        public static TradeEntities conObj;
+    }
+}
---
+++ Demo_var_6/Pages/PageAddProd.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo_var_6.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddProd.xaml
+    /// </summary>
+    public partial class PageAddProd : Page
+    {
+        public PageAddProd()
+        {
+            InitializeComponent();
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
